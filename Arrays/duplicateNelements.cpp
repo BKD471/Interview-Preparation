453e783352770d7b9a2fc4824b2639be7f4b02fa
BKD471 <bhaskarkumardas9@gmail.com>
2022-06-02 23:41:46 +0530

    day 4

---
+++ Arrays/duplicateNelements.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        int n=nums.size();
+        int res;
+        for(int i=0;i<n;i++){
+            
+            int index=abs(nums[i])-1;
+            nums[index]=-nums[index];
+            if(nums[index]>0){
+                res=i;
+                break;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            cout<<nums[i];
+        }
+        
+
+        for(int i=0;i<n;i++){
+            nums[i]=abs(nums[i]);
+        }
+        
+        return nums[res];
+        
+    }
+};
